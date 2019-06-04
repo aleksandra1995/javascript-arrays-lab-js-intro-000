0bad476ad17a58586e1d59ee637ea22dfdd70d45
Aleksandra Kosc <aleksandra.kosc@gmail.com>
2019-06-04 16:54:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ function prependKitten(name) {
   return ["Arnold",...kittens]
 }
 function removeLastKitten() {
-return kittens.slice(0, kittens.lenght)
+return kittens.slice(0, kittens.lenght -1)
 }
